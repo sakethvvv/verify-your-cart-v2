fe6dadd4d419a1f5d567269ac400591a4f594f7d
saketh <sakethvedullapalli@gmail.com>
2026-05-10 22:15:58 +0530

    Add use client to Footer for styled-jsx support

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from 'next/link';
 import { Send, MessageCircle, Camera, Globe, Shield, Mail, Phone, MapPin, Briefcase } from 'lucide-react';
 import CartLogo from './CartLogo';
